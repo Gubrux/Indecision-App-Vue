482ca938e1b6b92ca2f1b3961abdeeafc0025751
gabriel <gabrielcentur@gmail.com>
2024-08-21 13:35:48 -0400

    Add TypingIndicator component for displaying typing status in chat

---
+++ src/components/chat/TypingIndicator.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-start">
+        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+            <span>Escribiendo...</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
