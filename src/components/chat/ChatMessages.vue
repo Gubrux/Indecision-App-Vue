45f641079a1832aefc509e84b306e5e12f08c285
gabriel <gabrielcentur@gmail.com>
2024-08-21 12:28:12 -0400

    refactor: Update ChatMessages.vue to use ChatBubble component and TypeScript setup

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="flex-1 overflow-y-auto p-4">
         <div class="flex flex-col space-y-2">
-            <!-- Messages go here -->
-            <!-- Example Message -->
-            <div class="flex justify-end">
-                <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-                    Hey, how's your day going?
-                </div>
-            </div>
-
-            <!-- Example Received Message -->
-            <div class="flex">
-                <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-                    Not too bad, just a bit busy. How about you?
-                </div>
-            </div>
+            <ChatBubble
+                v-for="message in messages"
+                :key="message.id"
+                v-bind="message"
+            />
         </div>
     </div>
 </template>
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+import type { ChatMessagesInterface } from '../../interfaces/chat.messages.interface';
+
+interface Props {
+    messages: ChatMessagesInterface[];
+}
+defineProps<Props>();
+</script>
