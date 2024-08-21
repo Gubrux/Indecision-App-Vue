d559357db45d8a5f9e7f72666eabccad8efac5ec
gabriel <gabrielcentur@gmail.com>
2024-08-21 10:14:31 -0400

    feat: Update App.vue to use IndecisionView component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
-    <section class="flex justify-center items-center w-full h-screen">
-        <MyCounter :value="4" />
-        <MyCounterScript :value="8" />
-        <MyCounterScript2 :value="2" />
-    </section>
+    <IndecisionView />
 </template>
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
