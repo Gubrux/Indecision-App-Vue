ae93bea7b95f8bdcba1760738f2e335892bdd17b
gabriel <gabrielcentur@gmail.com>
2024-08-21 13:36:26 -0400

    Update IndecisionView.vue to use ChatMessages and MessageBox components

--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -1,15 +1,18 @@
-<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
 <template>
     <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
         <PersonBar />
 
-        <ChatMessages />
+        <ChatMessages :messages="messages" :isTyping="isTyping" />
 
-        <MessageBox />
+        <MessageBox @send-message="onMessage($event)" />
     </div>
 </template>
+
 <script setup lang="ts">
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import MessageBox from '@/components/chat/MessageBox.vue';
 import PersonBar from '@/components/chat/PersonBar.vue';
+import { useChat } from '@/composables/useChat';
+
+const { messages, onMessage, isTyping } = useChat();
 </script>
