292009ebf65d7e92cbd04219f96c2886b304a327
gabriel <gabrielcentur@gmail.com>
2024-08-21 16:04:25 -0400

    refactor:  improve scrolling behavior

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex-1 overflow-y-auto p-4">
+    <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
         <div class="flex flex-col space-y-2">
             <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
             <TypingIndicator v-if="isTyping" />
@@ -11,10 +11,21 @@
 import ChatBubble from '@/components/chat/ChatBubble.vue';
 import TypingIndicator from '@/components/chat/TypingIndicator.vue';
 import type { ChatMessagesInterface } from '../../interfaces/chat.messages.interface';
+import { ref, watch } from 'vue';
 
 interface Props {
     messages: ChatMessagesInterface[];
     isTyping: boolean;
 }
-defineProps<Props>();
+const chatRef = ref<HTMLDivElement | null>(null);
+const { messages } = defineProps<Props>();
+
+watch(messages, () => {
+    setTimeout(() => {
+        chatRef.value?.scrollTo({
+            top: chatRef.value?.scrollHeight,
+            behavior: 'smooth',
+        });
+    }, 100);
+});
 </script>
