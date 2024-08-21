285d1e59513d3200edb06d2bd79e545c0f592549
gabriel <gabrielcentur@gmail.com>
2024-08-21 13:35:32 -0400

    Update ChatMessages.vue to use ChatBubble component and TypingIndicator

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="flex-1 overflow-y-auto p-4">
         <div class="flex flex-col space-y-2">
-            <ChatBubble
-                v-for="message in messages"
-                :key="message.id"
-                v-bind="message"
-            />
+            <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
+            <TypingIndicator v-if="isTyping" />
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
-import ChatBubble from './ChatBubble.vue';
+import ChatBubble from '@/components/chat/ChatBubble.vue';
+import TypingIndicator from '@/components/chat/TypingIndicator.vue';
 import type { ChatMessagesInterface } from '../../interfaces/chat.messages.interface';
 
 interface Props {
     messages: ChatMessagesInterface[];
+    isTyping: boolean;
 }
 defineProps<Props>();
 </script>
